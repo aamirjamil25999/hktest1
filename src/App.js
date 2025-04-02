e10682c07c2eb554aa7702abb464c46568918d62
Aamir Jamil <aamirjamil@Aamirs-MacBook-Air.local>
2025-04-02 19:38:02 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import './App.css';
+import Dropdown from "./components/dropdown";
+import 'h8k-components';
+
+const title = "Employee Information";
+// this is the codebase //
+
+function App() {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
+  return (
+    <>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-center justify-content-center mt-50">
+        <section className="layout-column">
+          <div data-testid="country-options">
+            <Dropdown options={countryOptions} labelText={'Select Country'} onChange={handleCountryChange} />
+          </div >
+          <div data-testid="language-options">
+            <Dropdown options={languageOptions} labelText={'Select Language'} onChange={handleLanguageChange}/>
+          </div>
+          <label className="mt-50 text-align-center">
+          {selectedCountry ? `Selected Country: ${selectedCountry}` : "No country selected"}
+          </label>
+          <label className="mt-10 finalText" data-testid="country-selected">
+          {selectedLanguage ? `Selected Language: ${selectedLanguage}` : "No language selected"}
+          </label>
+          <label className="mt-10 finalText" data-testid="language-selected">
+            
+          </label>
+        </section>
+      </div>
+    </>
+  );
+}
+
+const countryOptions = [
+  'USA',
+  'Germany',
+  'France',
+  'Canada',
+  'India',
+  'Poland',
+  'Japan',
+  'Spain',
+  'Australia'
+];
+
+const languageOptions = [
+  'English',
+  'Spanish',
+  'French',
+  'German'
+]
+
+export default App;
---
+++ src/components/dropdown/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+function Dropdown ({options, labelText, onChange}) {
+
+  // console.log(options)
+
+  return (
+    <div className="elementSet">
+      <select data-testid="dropdown" defaultValue={labelText} onChange={onChange}>
+        {/* Do not remove this default option as this is needed in testing */}
+        {options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      {/* Do not remove this default option as this is needed in testing */}
+    </div>
+  );
+}
+
+export default Dropdown;
